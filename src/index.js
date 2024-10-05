e824edfd6c6683cf2bae0794a6af75dd8e149284
Glitch540 <marbz1234walkerz@gmail.com>
2024-10-05 15:09:30 -0700

    update
    javascript applied

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-{}
+function generatePoem(event) {
+    event.preventDefault();
+  
+    new Typewriter("#poem", {
+      strings: "Heroes rise with power bright, Iron, thunder, webs in flight.",
+      autoStart: true,
+      delay: 1,
+      cursor: "",
+    });
+  }
+  
+  let poemFormElement = document.querySelector("#poem-generator-form");
+  poemFormElement.addEventListener("submit", generatePoem);
+
+
+
+  
+  
